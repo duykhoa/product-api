568c731c5d6300c59105c2c58b58a1eb029d8a6c
Duy Khoa <duykhoa12t@gmail.com>
2017-01-04 12:29:38 +0800

    replace OPTIONS by PUT http method

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module ProductApi
 
     config.action_dispatch.default_headers = {
       'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+      'Access-Control-Request-Method' => %w{GET POST PUT}.join(",")
     }
   end
 end
